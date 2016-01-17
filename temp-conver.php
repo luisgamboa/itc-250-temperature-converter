247fe4d428ef0232b8fcd65970ad40269b723946
Luis Gamboa <luisgamboasierra@gmail.com>
2016-01-16 21:25:46 -0800

    temperature conversions
    it also includes error message

--- temp-conver.php
+++ temp-conver.php
@@ -1,129 +1,73 @@
-<link rel="stylesheet" href="style.css">
+<?php 
+//temp-conver.php
+//The app will allow users to convert temperature types, 
+//for example Fahrenheit to Celcius, Celcius to Fahrenheit and Fahrenheit to Kelvin
 
-<?php
-//temp-conver.php formulas
-//The app will allow users to convert temperature types, for example Fahrenheit to Celcius, Celcius to Fahrenheit and Fahrenheit to Kelvin
 define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 //echo THIS_PAGE;
 //die;
 
-
-    //Fahrenheit to Celsius formula
-    function fahrenheit_to_celsius($given_value)
-    {
-        $celsius=5/9*($given_value-32);
-        return $celsius ;
-    }
-    
-    //Fahrenheit to Kelvin formula
-    function fahrenheit_to_kelvin($given_value)
-    {
-        $kelvin=($given_value+459.67)*5/9;
-        return $kelvin ;
-    }
-
-    //Celsius to Fahrenheit formula
-    function celsius_to_fahrenheit($given_value)
-    {
-        $fahrenheit=$given_value*9/5+32;
-        return $fahrenheit ;
-    }
-
-    //Celsius to Kelvin formula
-    function celsius_to_kelvin($given_value)
-    {
-        $kelvin=$given_value+273.15;
-        return $kelvin ;
-    }
-
-    //Kelvin to Celsius formula
-    function kelvin_to_celsius($given_value)
-    {
-        $celsius=$given_value-273.15;
-        return $celsius ;
-    }
-
-    //Kelvin to Fahrenheit formula
-    function kelvin_to_fahrenheit($given_value)
-    {
-        $fahrenheit=($given_value-459.67)*5/9;
-        return $fahrenheit ;
-    }
+echo' 
+     <h2>Temperature Conversion</h2> 
+     
+     <form action = "' . THIS_PAGE . '" method = "GET"> 
+         Degrees: <input type = "text" name = "degree" size=4> 
+         <select name="temperatureType"> 
+             <option value="celcius">Celsius</option> 
+             <option value="fahrenheit">Fahrenheit</option> 
+             <option value="kelvin">Kelvin</option> 
+        </select> 
+         <br/> 
+         <br/> 
+         <input type = "submit" name = "Convert" value="Convert"/> 
+     </form>
+     
+     ';
 
 
+ //check for input from the user
+if (array_key_exists('degree',$_GET))
 
+ {
+	$typeOfTemperature = $_GET['temperatureType'];
+	$degreeValue = $_GET['degree'];
+	$firstLength = strlen($_GET['degree']);
+	
+	//it willcheck that input is not empty
+	if(($firstLength > 0) && (is_numeric($_GET['degree'])))
+		{
+            if ($typeOfTemperature == "celcius") 
+                {
+                     echo "<table><tr><th> Conversion Results</th></tr><tr><td>
+                     $degreeValue</td><td>Celsius</td></tr>"; 
+                     $celciusToFahrenheit = $degreeValue*9/5+32; 
+                     echo "<tr><td>$celciusToFahrenheit</td><td>Fahrenheit</td></tr>"; 
+                     $celciusToKelvin = $degreeValue+273.15; 
+                     echo "<tr><td>$celciusToKelvin </td><td>Kelvin</td></tr>"; 
+                }
 
-//html                  
-echo '
+            if ($typeOfTemperature == "fahrenheit") 
+                {
+                     echo "<table><tr><th> Conversion Results</th></tr><tr><td>
+                     $degreeValue</td><td>Farhenheit</td></tr>"; 
+                     $fahrenheitToCelcius = ($degreeValue -32)*5/9; 
+                     echo "<tr><td>$fahrenheitToCelcius</td><td>Celsius</td></tr>"; 
+                     $fahrenheitToKelvin = $fahrenheitToCelcius+273.15; 
+                     echo "<tr><td>$fahrenheitToKelvin </td><td>Kelvin</td></tr>"; 
+                } 
 
-        <form action="' . THIS_PAGE . '" method="post">
-        <table>
-            <tr>
-                <td>
-                    <select name="firstTemperatureType">
-                        <option value="fahrenheit">Fahrenheit</option>
-                        <option value="celsius">Celsius</option>
-                        <option value="kelvin">Kelvin</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="text" name="given_value">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <select name="secondTemperatureType">
-                        <option value="fahrenheit">Fahrenheit</option>
-                        <option value="celsius">Celsius</option>
-                        <option value="kelvin">Kelvin</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="submit" name="Submit" value="Convert">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                
-                </td>
-            </tr>
-        </table>
-        </form>
-        
-        
-';
-
-//results
-if(isset($_POST['Submit']))
-    
-{
-                        $firstTemperatureType=$_POST['firstTemperatureType'];
-                        $secondTemperatureType=$_POST['secondTemperatureType'];
-                        $given_value=$_POST['given_value'];
-    
-                        if($firstTemperatureType=='fahrenheit')
-                        {
-                            $celsious=fahrenheit_to_celsius($given_value);
-                            echo "Fahrenheit $given_value = $celsious Celsious";
-                        }
-    
-    
-
-    
-    
-                        elseif($firstTemperatureType=='fahrenheit')
-                        {
-                            $kelvin=fahrenheit_to_kelvin($given_value);
-                            echo "Fahrenheit  $given_value = $kelvin Kelvin";
-                        }
-                        
-    
-
+             if ($typeOfTemperature == "kelvin") 
+                {
+                     echo "<table><tr><th> Conversion Results</th></tr><tr><td> 
+                     $degreeValue</td><td>Kelvin</td></tr>"; 
+                     $kelvinToFahrenheit = ($degreeValue - 273.15) * 9 / 5 + 32; 
+                     echo "<tr><td>$kelvinToFahrenheit</td><td>Fahrenheit</td></tr>"; 
+                     $kelvinToCelcius = $degreeValue-273.15; 
+                     echo "<tr><td>$kelvinToCelcius </td><td>Celsius</td></tr>"; 
+                } 
+		 
+	 }
+	 else
+		//echo an error message if input is empty
+		echo "<span style = \"color:red\">*Please enter a valid temperature value.</span>";
 }
-
-
-
